d83d31e5cee98517297da8d8a6c58fe9993a075b
vitalik poliakov <vitalik211287@gmail.com>
2022-03-04 17:46:50 +0200

    animatoon aos

--- src/index.js
+++ src/index.js
@@ -1,29 +1,13 @@
+
 import './sass/main.scss';
+
 import './animate.css';
-// import "./js/about-modal";
-import "./js/poduct-modal";
+import './js/poduct-modal';
 import './js/hero-modal';
-import "./js/menu";
-import "./js/header-modal";
-
-
-
-import "./js/modal";
-import "./js/slider";
-
-// import "./js/modal";
-import "./js/mobile-menu";
-
-import "./js/wow.min";
-import "./js/contacts";
-
-
-
-
-
-
-
-
-
-
-
+import './js/menu';
+import './js/header-modal';
+import './js/modal';
+import './js/slider';
+import './js/mobile-menu';
+import './js/contacts';
+import './js/wow.min';
